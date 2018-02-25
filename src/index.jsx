8915317fdfd466493dfc2bbca75a62172d443e23
Sandeep Gupta <sangupta@adobe.com>
2018-02-25 13:25:37 -0800

    adding index.jsx to export components

---
+++ src/index.jsx
@@ -0,0 +1,28 @@
+import InfinteScroll from './components/InfinteScroll';
+
+import WindowSizeAware from './components/layout/WindowSizeAware';
+import DesktopMode from './components/layout/DesktopMode';
+import LaptopMode from './components/layout/LaptopMode';
+import PhoneMode from './components/layout/PhoneMode';
+import TabletMode from './components/layout/TabletMode';
+import MobileMode from './components/layout/MobileMode';
+
+export default {
+    InfinteScroll,
+    WindowSizeAware,
+    DesktopMode,
+    LaptopMode,
+    PhoneMode,
+    TabletMode,
+    MobileMode
+}
+
+export {
+    InfinteScroll,
+    WindowSizeAware,
+    DesktopMode,
+    LaptopMode,
+    PhoneMode,
+    TabletMode,
+    MobileMode
+}
