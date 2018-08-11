c12dbc5bdc98ec3f9f072ac51de0114f7d7be280
Sandeep Gupta <sangupta@users.noreply.github.com>
2018-08-11 14:42:00 -0700

    Create Alert.jsx

---
+++ src/components/Alert.jsx
@@ -0,0 +1,17 @@
+/**
+ * Simple alert component based on BS
+ *
+ * Ref: http://getbootstrap.com/docs/4.1/components/alerts/
+ */
+@Component
+export default class Alert {
+
+  @Attribute variant;
+  
+  render() {
+    return <div class={ 'alert alert-' + this.variant}>
+      { this.children }
+    </div>;
+  }
+
+}
