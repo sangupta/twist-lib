e2ddcfb4873974b90ca97a606de5cb01e2671b39
Sandeep Gupta <sangupta@users.noreply.github.com>
2018-08-11 14:55:03 -0700

    Added block, active, disabled modes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,19 +6,50 @@
 @Component
 export default class Button {
 
-  @Attribute variant;
+  @Attribute variant:string = '';
   
-  @Attribute isOutline = false;
-  
+  @Attribute outline:boolean = false;
+
+  @Attribute size:string = '';
+
+  @Attribute block:boolean = false;
+
+  @Attribute active:boolean = false;
+
+  @Attribute disabled:boolean = false;
+
   render() {
     let classes = 'btn ';
-    if(this.isOutline) {
+    
+    // take care of outline
+    if(this.outline) {
       classes = classes + 'btn-outline-' + this.variant;
     } else {
       classes = classes + 'btn-' + this.variant;
     }
     
-    return <button type='button' class={ classes }>
+    // take care of size - it can be empty, lg or sm
+    if(this.size.length > 0) {
+      classes = classes + ' btn-' + this.size;
+    }
+    
+    // take care of block level button
+    if(this.block) {
+      classes = classes + ' btn-block';
+    }
+    
+    // take care of active state
+    if(this.active) {
+      classes = classes + ' active';
+    }
+    
+    // take care of disabled state
+    let disabledString:string = '';
+    if(this.disabled) {
+      disabledString = 'disabled';
+    }
+    
+    return <button type='button' class={ classes } { disabledString }>
       { this.children }
     </button>;
 
