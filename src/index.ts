6634ae51ac19dd3362e3eb5a3b80785e134d73e8
Sandeep Gupta <sangupta@adobe.com>
2018-08-11 16:32:51 -0700

    add components to export

--- src/index.ts
+++ src/index.ts
@@ -7,22 +7,51 @@ import PhoneMode from './components/layout/PhoneMode';
 import TabletMode from './components/layout/TabletMode';
 import MobileMode from './components/layout/MobileMode';
 
+import Alert from 'components/Alert';
+import Badge from 'components/Badge';
+import Button from 'components/Button';
+import ButtonGroup from 'components/ButtonGroup';
+import ButtonToolbar from 'components/ButtonToolbar';
+import Jumbotron from 'components/Jumbotron';
+import LinkButton from 'components/LinkButton';
+import ProgressBar from 'components/ProgressBar';
+
 export default {
     InfinteScroll,
+    
     WindowSizeAware,
     DesktopMode,
     LaptopMode,
     PhoneMode,
     TabletMode,
-    MobileMode
+    MobileMode,
+
+    Alert,
+    Badge,
+    Button,
+    ButtonGroup,
+    ButtonToolbar,
+    Jumbotron,
+    LinkButton,
+    ProgressBar
 }
 
 export {
     InfinteScroll,
+
     WindowSizeAware,
     DesktopMode,
     LaptopMode,
     PhoneMode,
     TabletMode,
-    MobileMode
+    MobileMode,
+
+    Alert,
+    Badge,
+    Button,
+    ButtonGroup,
+    ButtonToolbar,
+    Jumbotron,
+    LinkButton,
+    ProgressBar
 }
