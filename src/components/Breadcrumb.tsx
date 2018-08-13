b05a4846ff8bb0518436999675355edd9688caab
Sandeep Gupta <sangupta@adobe.com>
2018-08-13 13:41:39 -0700

    move breadcrumb classes to one file

--- src/components/Breadcrumb.tsx
+++ src/components/Breadcrumb.tsx
@@ -1,7 +1,7 @@
 import { Component, BaseComponent } from "@twist/react";
 
 @Component
-export default class Breadcrumb extends BaseComponent {
+export class Breadcrumb extends BaseComponent {
 
     render() {
         return <nav aria-label="breadcrumb">
@@ -9,6 +9,19 @@ export default class Breadcrumb extends BaseComponent {
                 { this.children }
             </ol>
         </nav>;
+    }
+
+}
+
+@Component
+export class BreadcrumbItem extends BaseComponent {
+
+    @Attribute active:boolean = false;
+
+    render() {
+        return <li class='breadcrumb-item' class-active={ this.active }>
+            { this.children }
+        </li>;
     }
 
 }
