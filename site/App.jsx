a1ba9bfa34f609724e74d313c343ee8b0c14b39f
Sandeep Gupta <sangupta@adobe.com>
2018-08-13 11:59:56 -0700

    add basic site infra

--- site/App.jsx
+++ site/App.jsx
@@ -7,9 +7,49 @@ import Alert from 'components/Alert';
 class App {
 
     render() {
-        return <Alert variant='primary'>
-            Hello World!
-        </Alert>;
+        return <g>
+            <header class='mb-auto'>
+                <nav class="navbar navbar-expand-md fixed-top">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="/">TwistLib</a>
+                        <div class="collapse navbar-collapse" id="navbarCollapse">
+                            <ul class="navbar-nav mr-auto">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="/">Components</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+            <main role='main' class='container-fluid'>
+                <div class='row flex-xl-nowrap'>
+                    <div class='col-12 col-md-3 col-xl-2 sidebar'>
+                        <ul class='nav flex-column'>
+                            <li class='nav-item'><a class="nav-link" href='#'>Alert</a></li>
+                            <li class='nav-item'><a class="nav-link" href='#'>Badge</a></li>
+                            <li class='nav-item'><a class="nav-link" href='#'>Breadcrumbs</a></li>
+                            <li class='nav-item'><a class="nav-link" href='#'>Button</a></li>
+                            <li class='nav-item'><a class="nav-link" href='#'>Button Group</a></li>
+                            <li class='nav-item'><a class="nav-link" href='#'>Button Toolbar</a></li>
+                            <li class='nav-item'><a class="nav-link" href='#'>Infinite Scroll</a></li>
+                            <li class='nav-item'><a class="nav-link" href='#'>Jumbotron</a></li>
+                            <li class='nav-item'><a class="nav-link" href='#'>Link Button</a></li>
+                            <li class='nav-item'><a class="nav-link" href='#'>Progress Bar</a></li>
+                        </ul>
+                    </div>
+                    <div class='col doc-container'>
+                    </div>
+                </div>
+            </main>
+            <footer class='mt-auto'>
+                <div class='row'>
+                    <div class='col'>
+                        TwistLib Documentation. Powered by React, TwistLib and Boostrap.
+                    </div>
+                </div>
+            </footer>
+        </g>;
     }
 
 }
